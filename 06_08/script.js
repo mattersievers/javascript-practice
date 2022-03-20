1376ff0a1d27ee49b214474aa59b286b81497859
Matthew Sievers <MatterSievers@gmail.com>
2022-03-20 15:51:05 -0500

    ch 6 completed

--- 06_08/script.js
+++ 06_08/script.js
@@ -5,12 +5,31 @@
 
 let a = 5;
 let b = 4;
-let c = 3.2;
+let c = "3.2";
 
 console.log(`let a: ${a} (${typeof a})`);
 console.log(`let b: ${b} (${typeof b})`);
 console.log(`let c: ${c} (${typeof c})`);
 
-let result = a + b;
+//Adding a string to 
+let result1 = a + b + c;
 
-console.log("Result: ", result);
+console.log("Result 1: ", result1, ` type of ${typeof result1}`);
+
+
+let result2 = a + b + parseFloat(c);
+
+console.log("Result 2: ", result2, ` type of ${typeof result2}`);
+
+
+
+//exponents a = 5 to the power of b = 4
+let result3 = a ** b;
+
+console.log("Result 3: ", result3, ` type of ${typeof result3}`);
+
+
+//modulo arithmetic 5 divides 4 (remainder of 1)
+let result4 = a % b
+
+console.log("Result 4: ", result4, ` type of ${typeof result4}`);
